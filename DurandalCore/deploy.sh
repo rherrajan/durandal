802c2dfddfd0667eb65b50b0eb375ad26448b672
rherrajan <annain@unterderbruecke.de>
2015-05-31 15:47:02 +0200

    als Bibliothek benutzbar

---
+++ DurandalCore/deploy.sh
@@ -0,0 +1,7 @@
+mvn install:install-file \
+    -DgroupId=tk.icudi.durandal \
+    -DartifactId=durandal-core \
+    -Dpackaging=jar \
+    -Dversion=0.1 \
+    -Dfile=bin/durandalcore.jar \
+    -DgeneratePom=true
--- DurandalCore/src/tk/icudi/durandal/core/DurandalCoreActivity.java
+++ DurandalCore/src/tk/icudi/durandal/core/DurandalCoreActivity.java
@@ -32,7 +32,7 @@ public class DurandalCoreActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
-		setContentView(R.layout.activity_fullscreen);
+//		setContentView(R.layout.activity_fullscreen);
 		
 	    Intent intent = getIntent();
 
