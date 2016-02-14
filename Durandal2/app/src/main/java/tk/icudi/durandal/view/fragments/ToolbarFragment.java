cc7a458d28820daaeebe217d65e2ac21e0b8e738
rherrajan <annain@unterderbruecke.de>
2016-02-14 14:25:33 +0100

    Toolbar hinzugefügt

--- Durandal2/app/src/main/java/tk/icudi/durandal/view/fragments/ToolbarFragment.java
+++ Durandal2/app/src/main/java/tk/icudi/durandal/view/fragments/ToolbarFragment.java
@@ -2,8 +2,10 @@ package tk.icudi.durandal.view.fragments;
 
 import android.app.Activity;
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -12,6 +14,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import tk.icudi.durandal.R;
+import tk.icudi.durandal.bluetooth.DeviceListActivity;
 
 
 public class ToolbarFragment extends Fragment {
@@ -36,8 +39,8 @@ public class ToolbarFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         this.toolbar = inflater.inflate(R.layout.fragment_toolbar, container, false);
-//        Toolbar toolbar = (Toolbar) this.toolbar.findViewById(R.id.nearby_toolbar);
-//        activity.setSupportActionBar(toolbar);
+        Toolbar toolbar = (Toolbar) this.toolbar.findViewById(R.id.main_toolbar);
+        activity.setSupportActionBar(toolbar);
 
         return this.toolbar;
     }
@@ -47,12 +50,22 @@ public class ToolbarFragment extends Fragment {
         super.onCreateOptionsMenu(menu, inflater);
 
         this.menu = menu;
-        // inflater.inflate(R.menu.main_activity_actions, menu);
+        inflater.inflate(R.menu.main, menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+
+        System.out.println(" --- item.getItemId(): " + item.getItemId());
+
         switch (item.getItemId()) {
+
+            case R.id.action_bluetooth: {
+                // Launch the DeviceListActivity to see devices and do scan
+                Intent serverIntent = new Intent(getActivity(), DeviceListActivity.class);
+                startActivityForResult(serverIntent, DeviceListActivity.REQUEST_CONNECT_DEVICE_SECURE);
+                return true;
+            }
 
             default:
                 return super.onOptionsItemSelected(item);
